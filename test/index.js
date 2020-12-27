2b5d88af32847c2c7ec216a422ce3f1678a24856
Glitch (sideways-rattle-snickerdoodle) <none>
2020-12-27 07:28:25 +0000

    🎬🚀 Updated with Glitch

--- test/index.js
+++ test/index.js
@@ -1,27 +1,29 @@
 /* eslint-env node, mocha */
-import wr from '../lib'
-import chai from 'chai'
-import chaiAsPromised from 'chai-as-promised'
+const wr = require('../lib')
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
 chai.use(chaiAsPromised)
-var expect = chai.expect
+const expect = chai.expect
 
 describe('wordreference-api', function () {
   this.timeout(5000)
-  describe('When you use a no valid word', function () {
+  describe('When you use an invalid word', function () {
     it('Should throw an error')
   })
-  describe('When you use a no valid language', function () {
+  describe('When you use an invalid language', function () {
     it('Should throw an error')
   })
   describe('When you use a valid word', function () {
     it('Should return a JSON with some properties', function (done) {
-      expect(wr('rainbow')).to.eventually.have.property('word')
+      expect(wr('rainbow'))
+        .to.eventually.have.property('word')
         .and.notify(done)
     })
   })
   describe('When you use a valid word and valid languages', function () {
     it('Should return a JSON with some properties', function (done) {
-      expect(wr('rainbow')).to.eventually.have.property('word')
+      expect(wr('rainbow'))
+        .to.eventually.have.property('word')
         .and.notify(done)
     })
   })
