f075fd1b986b910104b8545c2f07d9ac11eef662
Glitch (sideways-rattle-snickerdoodle) <none>
2020-12-27 06:28:42 +0000

    📕📚 Updated with Glitch

--- test/manual-test.js
+++ test/manual-test.js
@@ -1,5 +1,5 @@
 const wr = require('./../dist')
 const fs = require('fs')
 wr('rainbow')
-.then(res => fs.writeFile('example.json',JSON.stringify(res,undefined,2)))
-.catch(res => console.log(res)) 
+.then(res => fs.writeFile('example.json', JSON.stringify(res, undefined, 2)))
+.catch(res => console.log(res))
